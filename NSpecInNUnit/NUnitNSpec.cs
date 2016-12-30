10f6a1d13d33eeb30068cf033bf9a50e35f54f09
Per Rovegård <per@rovegard.se>
2016-12-30 01:01:16 +0100

    Update NUnitNSpec

--- NSpecInNUnit/NUnitNSpec.cs
+++ NSpecInNUnit/NUnitNSpec.cs
@@ -6,66 +6,58 @@ using NSpec;
 using NSpec.Domain;
 using NSpec.Domain.Formatters;
 using NUnit.Framework;
-using NUnit.Framework.Interfaces;
 
 namespace NSpecInNUnit
 {
-    public abstract class NUnitNSpec<T> : nspec
+    /// <summary>
+    /// Base class for NSpec-based tests. This class allows us to run tests in Visual Studio using
+    /// ReSharper as well as from the console using the NUnit runner.
+    /// </summary>
+    public abstract class NUnitNSpec : nspec
     {
-        /// <summary>
-        /// Test case source for the example runner test. This method generates a NUnit compatible
-        /// test object for each discovered example. It's not a good idea to actually run the examples in
-        /// this method, since it is called as part of NUnit's dicovery phase, which happens before setup
-        /// fixtures are run.
-        /// </summary>
-        public static IEnumerable Examples()
+        private bool _hasRun;
+
+        public IEnumerable Examples()
         {
-            var currentSpec = typeof (T);
+            var currentSpec = GetType();
             var finder = new SpecFinder(new[] { currentSpec });
-            var builder = new ContextBuilder(finder, new Tags().Parse(currentSpec.Name), new DefaultConventions());
+            var tagsFilter = new Tags().Parse(currentSpec.Name);
+            var builder = new ContextBuilder(finder, tagsFilter, new DefaultConventions());
             try
             {
-                var contextCollection = builder.Contexts().Build();
-                var examples = contextCollection.SelectMany(ctx => ctx.AllExamples());
-                return examples.Select(example => TestCaseDataFromExample(example, finder));
+                // The test case source is run during the discovery phase of NUnit. This means that we cannot
+                // run the examples now - setup fixtures haven't run yet, for example. But we use the result
+                // of the discovery phase to produce a number of test cases.
+                var testSuite = builder.Contexts().Build();
+                var examples = testSuite.SelectMany(ctx => ctx.AllExamples());
+                return examples.Select(example => new NUnitTestFromExample(tagsFilter, testSuite, example));
             }
             catch (Exception ex)
             {
-                var example = new Example("Example discovery failure");
-                example.Context = new Context("example discovery failure"); // for Example.FullName() to work
+                // Generate a fake example that represents the setup failure.
+                var example = new Example("Test setup failure");
+                example.Context = new Context("test failure"); // for Example.FullName() to work
                 example.Exception = ex;
-                return new object[] { TestCaseDataFromExample(example, finder) };
+                example.HasRun = true;
+                return new object[] { new NUnitTestFromExample(tagsFilter, null, example) };
             }
         }
 
-        private static ITestCaseData TestCaseDataFromExample(ExampleBase example, SpecFinder finder)
-        {
-            var tcd = new TestCaseData(example, finder).SetName(example.FullName());
-            if (example.Pending) tcd = tcd.Explicit();
-            return tcd;
-        }
-
         [TestCaseSource(nameof(Examples))]
-        public void RunExample(ExampleBase example, SpecFinder finder)
+        public void RealizeSpec(ExampleContext ctx)
         {
-            var coll = new ContextCollection(new[] { example.Context });
+            // If we haven't run the examples yet, do so now. TestSuite is null in case of setup failure. All
+            // my attempts to run examples individually have failed (e.g. due to nested contexts or the use of
+            // beforeAll), so run everything and use the individual test cases only for reporting.
+            if (!_hasRun && ctx.TestSuite != null)
+            {
+                _hasRun = true;
+                var runner = new ContextRunner(ctx.TagsFilter, new NoopFormatter(), false);
+                runner.Run(ctx.TestSuite);
+            }
 
-            // Unsuccessful attempt to run one example per test:
-            // var aTag = Guid.NewGuid().ToString();
-            // example.Tags.Add(aTag);
-            // ...and then `new Tags().Parse(aTag)` below.
-
-            // A dummy ContextBuilder to get Tags into the ContextRunner
-            var dummyBuilder = new ContextBuilder(finder, new Tags(), new DefaultConventions());
-            var runner = new ContextRunner(dummyBuilder, new NoopFormatter(), false);
-
-            var runCountBefore = example.Context.AllExamples().Count(_ => _.HasRun);
-            runner.Run(coll);
-            var runCountAfter = example.Context.AllExamples().Count(_ => _.HasRun);
-
-            var examplesRun = runCountAfter - runCountBefore;
-            if (examplesRun != 1) throw new Exception($"Expected 1 example to run, not {examplesRun}.");
-
+            var example = ctx.Example;
+            if (!example.HasRun) throw new Exception("The example didn't run :-(");
             if (example.Failed())
             {
                 if (example.Exception != null)
@@ -74,15 +66,50 @@ namespace NSpecInNUnit
                     var edi = ExceptionDispatchInfo.Capture(example.Exception);
                     edi.Throw();
                 }
-                throw new Exception("It failed :-(");
+                throw new Exception("The example failed, but I don't know why :-(");
             }
         }
     }
 
+    /// <summary>
+    /// An NSpec formatter that doesn't do anything.
+    /// </summary>
     public class NoopFormatter : IFormatter
     {
         public void Write(ContextCollection contexts)
         {
+        }
+    }
+
+    /// <summary>
+    /// An NUnit wrapper for NSpec information that at the same time acts as a single test case from a
+    /// test case source.
+    /// </summary>
+    public class NUnitTestFromExample : TestCaseData
+    {
+        public NUnitTestFromExample(Tags tagsFilter, ContextCollection testSuite, ExampleBase example)
+            : base(new ExampleContext(tagsFilter, testSuite, example))
+        {
+            if (example.Pending) Ignore();
+            SetName(example.FullName());
+        }
+    }
+
+    /// <summary>
+    /// Contains an NSpec example instance toghether with contextual information (instances) necessary
+    /// to run the example.
+    /// </summary>
+    public class ExampleContext
+    {
+        public Tags TagsFilter { get; }
+        public ContextCollection TestSuite { get; }
+        public ExampleBase Example { get; }
+
+        public ExampleContext(Tags tagsFilter, ContextCollection testSuite, ExampleBase example)
+        {
+            TagsFilter = tagsFilter;
+            TestSuite = testSuite;
+            Example = example;
         }
     }
 }
