368199a0535615b86cb4d3a6612164b6146ea867
Per Rovegård <per@rovegard.se>
2016-12-30 01:09:25 +0100

    Add a coverage test example

---
+++ NSpecInNUnit/cover.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NSpec;
+
+namespace NSpecInNUnit
+{
+    // Run tests with something like DotCover (https://www.jetbrains.com/dotcover/)
+    // Target.Sub shouldn't be covered, the other should be.
+    public class cover : NUnitNSpec
+    {
+        public void describe_Target()
+        {
+            Target target = null;
+            beforeAll = () => target = new Target();
+
+            it["should add"] = () => target.Add(2, 3).should_be(5);
+            it["should multiply"] = () => target.Mul(2, 3).should_be(6);
+        }
+    }
+
+    public class Target
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Mul(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
