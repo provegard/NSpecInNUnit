66af2ae99b553625983903f7c463f11e103c32fe
Per Rovegård <per@rovegard.se>
2017-11-12 23:08:26 +0100

    Adding an example project based on .NET 4.5.2

---
+++ NSpecInNUnit.Examples.452/a_spec.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+namespace NSpecInNUnit
+{
+    public class a_spec : nspec_as_nunit<a_spec>
+    {
+        public void describe_stuff()
+        {
+            it["should run in a .NET 4.5.2 project"] = () => Assert.That(1, Is.EqualTo(1));
+        }
+    }
+}
